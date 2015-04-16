f70f0b29ee30182939c20e08883e755e63176ce2
Joao Duarte <jsvduarte@gmail.com>
2015-04-16 11:10:00 +0100

    remove ROOT_DIR constant from jars loading file
    since this code is present on all plugins that contain jars, each
    one will define ROOT_DIR, causing warnings

--- lib/logstash-output-elasticsearch_jars.rb
+++ lib/logstash-output-elasticsearch_jars.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 require 'logstash/environment'
 
-ROOT_DIR   = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-LogStash::Environment.load_runtime_jars! File.join(ROOT_DIR, "vendor")
+root_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+LogStash::Environment.load_runtime_jars! File.join(root_dir, "vendor")
