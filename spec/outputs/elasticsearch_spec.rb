168fd92b4a0adec6f48ac72912381a3272de4020
Joao Duarte <jsvduarte@gmail.com>
2015-06-04 15:09:01 +0100

    add the option to disable certificate verification
    Fixes #167

--- spec/outputs/elasticsearch_spec.rb
+++ spec/outputs/elasticsearch_spec.rb
@@ -900,6 +900,34 @@ describe "ship lots of events w/ default index_type and dynamic routing key usin
         end
       end
     end
+
+    context "when using http protocol" do
+      protocol = "http"
+      context "when using ssl without cert verification" do
+        subject do
+          require "logstash/outputs/elasticsearch"
+          settings = {
+            "protocol" => protocol,
+            "host" => "node01",
+            "ssl" => true,
+            "ssl_certificate_verification" => false
+          }
+          next LogStash::Outputs::ElasticSearch.new(settings)
+        end
+
+        it "should pass the flag to the ES client" do
+          expect(::Elasticsearch::Client).to receive(:new) do |args|
+            expect(args[:ssl]).to eq(:verify => false)
+          end
+          subject.register
+        end
+
+        it "print a warning" do
+          expect(subject.logger).to receive(:warn)
+          subject.register
+        end
+      end
+    end
   end
 
   describe "send messages to ElasticSearch using HTTPS", :elasticsearch_secure => true do
