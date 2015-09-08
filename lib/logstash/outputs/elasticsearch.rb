26736660113bf42cb0b7683e62c88873e9d3e74f
Andrew Cholakian <andrew@andrewvc.com>
2015-09-08 12:50:16 -0500

    Restore @port setting
    Fixes #247

--- lib/logstash/outputs/elasticsearch.rb
+++ lib/logstash/outputs/elasticsearch.rb
@@ -254,7 +254,7 @@ class LogStash::Outputs::ElasticSearch < LogStash::Outputs::Base
     common_options.merge! update_options if @action == 'update'
 
     @client = LogStash::Outputs::Elasticsearch::HttpClient.new(
-      common_options.merge(:hosts => @hosts)
+      common_options.merge(:hosts => @hosts, :port => @port)
     )
 
     if @manage_template
