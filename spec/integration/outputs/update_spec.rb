747fdcc2b725de76caeca59753b11beb656f3414
David Chauviere <d_chauviere@yahoo.fr>
2015-11-25 21:34:30 +0100

    Repair tests after rebase

--- spec/integration/outputs/update_spec.rb
+++ spec/integration/outputs/update_spec.rb
@@ -32,11 +32,11 @@ describe "Update actions", :integration => true do
   end
 
   it "should fail without a document_id" do
-    event = LogStash::Event.new("somevalue" => 100, "@timestamp" => "2014-11-17T20:37:17.223Z", "@metadata" => {"retry_count" => 0})
-    action = ["update", {:_id=>nil, :_index=>"logstash-2014.11.17", :_type=>"logs"}, event]
-    subject = get_es_output
+    subject = get_es_output()
     subject.register
-    expect { subject.flush([action]) }.to raise_error
+    subject.receive(LogStash::Event.new("somevalue" => 100))
+    expect(subject.logger).to receive(:warn).at_least(1).with(anything,hash_including(:class => 'LogStash::ConfigurationError'))
+    subject.flush
   end
 
   context "when update only" do
@@ -63,7 +63,7 @@ describe "Update actions", :integration => true do
       subject = get_es_output({ 'document_id' => "123", 'script' => 'scripted_update' })
       subject.register
       subject.receive(LogStash::Event.new("count" => 2))
-      subject.flush(:final => true)
+      subject.flush
       r = @es.get(:index => 'logstash-update', :type => 'logs', :id => "123", :refresh => true)
       insist { r["_source"]["counter"] } == 3
     end
@@ -72,7 +72,7 @@ describe "Update actions", :integration => true do
       subject = get_es_output({ 'document_id' => "123", 'script' => 'scripted_update_nested' })
       subject.register
       subject.receive(LogStash::Event.new("data" => { "count" => 3 }))
-      subject.flush(:final => true)
+      subject.flush
       r = @es.get(:index => 'logstash-update', :type => 'logs', :id => "123", :refresh => true)
       insist { r["_source"]["counter"] } == 4
     end
@@ -86,7 +86,7 @@ describe "Update actions", :integration => true do
       })
       subject.register
       subject.receive(LogStash::Event.new("count" => 3 ))
-      subject.flush(:final => true)
+      subject.flush
       r = @es.get(:index => 'logstash-update', :type => 'logs', :id => "123", :refresh => true)
       insist { r["_source"]["counter"] } == 4
     end
@@ -97,7 +97,7 @@ describe "Update actions", :integration => true do
       subject = get_es_output({ 'document_id' => "456", 'upsert' => '{"message": "upsert message"}' })
       subject.register
       subject.receive(LogStash::Event.new("message" => "sample message here"))
-      subject.flush(:final => true)
+      subject.flush
       r = @es.get(:index => 'logstash-update', :type => 'logs', :id => "456", :refresh => true)
       insist { r["_source"]["message"] } == 'upsert message'
     end
@@ -106,7 +106,7 @@ describe "Update actions", :integration => true do
       subject = get_es_output({ 'document_id' => "456", 'doc_as_upsert' => true })
       subject.register
       subject.receive(LogStash::Event.new("message" => "sample message here"))
-      subject.flush(:final => true)
+      subject.flush
       r = @es.get(:index => 'logstash-update', :type => 'logs', :id => "456", :refresh => true)
       insist { r["_source"]["message"] } == 'sample message here'
     end
@@ -116,7 +116,7 @@ describe "Update actions", :integration => true do
         subject = get_es_output({ 'document_id' => "456", 'script' => 'scripted_update', 'upsert' => '{"message": "upsert message"}' })
         subject.register
         subject.receive(LogStash::Event.new("message" => "sample message here"))
-        subject.flush(:final => true)
+        subject.flush
         r = @es.get(:index => 'logstash-update', :type => 'logs', :id => "456", :refresh => true)
         insist { r["_source"]["message"] } == 'upsert message'
       end
@@ -125,7 +125,7 @@ describe "Update actions", :integration => true do
         subject = get_es_output({ 'document_id' => "456", 'script' => 'scripted_upsert', 'scripted_upsert' => true })
         subject.register
         subject.receive(LogStash::Event.new("counter" => 1))
-        subject.flush(:final => true)
+        subject.flush
         r = @es.get(:index => 'logstash-update', :type => 'logs', :id => "456", :refresh => true)
         insist { r["_source"]["counter"] } == 1
       end
