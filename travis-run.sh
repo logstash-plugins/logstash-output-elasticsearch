68961317b39ee94ef561579d9109906c5261f7c7
Suyog Rao <suyog.rao@gmail.com>
2016-04-26 15:59:13 -0700

    Add 5.0.0-alpha1 to tests
    Add some functions to travis.sh so we can use multiple
    ES versions
    
    Fixes #419

--- travis-run.sh
+++ travis-run.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 set -ex
 
-if [[ "$INTEGRATION" != "true" ]]; then
-  bundle exec rspec -fd spec
-else
-  curl -s https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.tar.gz > elasticsearch.tar.gz
+setup_es() {
+  download_url=$1
+  curl -sL $download_url > elasticsearch.tar.gz
   mkdir elasticsearch
   tar -xzf elasticsearch.tar.gz --strip-components=1 -C ./elasticsearch/.
   ln -sn ../../spec/fixtures/scripts elasticsearch/config/.
-  elasticsearch/bin/elasticsearch -Des.script.inline=on -Des.script.indexed=on -Des.script.file=on > /tmp/elasticsearch.log &
+}
+
+start_es() {
+  es_args=$1
+  elasticsearch/bin/elasticsearch $es_args > /tmp/elasticsearch.log &
   sleep 10
   curl http://localhost:9200 && echo "ES is up!" || cat /tmp/elasticsearch.log
-  bundle exec rspec -fd spec --tag integration || cat /tmp/elasticsearch.log
+}
+
+if [[ "$INTEGRATION" != "true" ]]; then
+  bundle exec rspec -fd spec
+else
+  if [[ "$ES_VERSION" == 5.* ]]; then
+    setup_es https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$ES_VERSION/elasticsearch-$ES_VERSION.tar.gz
+    start_es -Ees.script.inline=true -Ees.script.indexed=true -Ees.script.file=true
+    bundle exec rspec -fd spec --tag integration --tag version_5.x || cat /tmp/elasticsearch.log
+  else
+    setup_es https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.tar.gz
+    start_es -Des.script.inline=on -Des.script.indexed=on -Des.script.file=on
+    bundle exec rspec -fd spec --tag integration || cat /tmp/elasticsearch.log
+  fi
 fi
