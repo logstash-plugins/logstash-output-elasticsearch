83cbaa76832a921fc02e46739861ee3a66e8312d
Jordan Sissel <jls@semicomplete.com>
2016-09-13 16:17:03 -0700

    Add comment for what the without_credentials is supposed to do

--- lib/logstash/outputs/elasticsearch/safe_url.rb
+++ lib/logstash/outputs/elasticsearch/safe_url.rb
@@ -3,6 +3,9 @@ module LogStash; module Outputs; class ElasticSearch;
     PLACEHOLDER = "~hidden~".freeze
 
     module_function
+
+    # Takes a URI object and returns a copy of it with any user or password
+    # information replaced with a placeholder `~hidden~`.
     def without_credentials(url)
       url.dup.tap do |u|
         u.user = PLACEHOLDER if u.user
