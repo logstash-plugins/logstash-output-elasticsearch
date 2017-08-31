a59ffc40494c448ede2d31377562b528775d1cb1
Rob Bavey <rob.bavey@elastic.co>
2017-08-31 15:05:22 -0400

    Disable es tests for groovy scripts
    Support for groovy scripts is removed for ES 6+, so ensure that the
    tests don't run in that configuration.

--- spec/es_spec_helper.rb
+++ spec/es_spec_helper.rb
@@ -11,7 +11,7 @@ module ESHelper
     Elasticsearch::Client.new(:hosts => [get_host_port])
   end
 
-  def self.es_version_satisfies?(requirement)
+  def self.es_version_satisfies?(*requirement)
     es_version = RSpec.configuration.filter[:es_version] || ENV['ES_VERSION']
     es_release_version = Gem::Version.new(es_version).release
     Gem::Requirement.new(requirement).satisfied_by?(es_release_version)
