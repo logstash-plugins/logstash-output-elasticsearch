5183135e045cd7d73e8927f3bb6e1f06d6627b15
Joao Duarte <jsvduarte@gmail.com>
2020-03-27 15:05:25 +0000

    update to centralized travis configuration

--- .ci/docker-run.sh
+++ .ci/docker-run.sh
@@ -2,8 +2,11 @@
 
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
+
+cd .ci
+
 if [ "$INTEGRATION" == "true" ]; then
-    docker-compose -f ci/docker-compose.yml up --exit-code-from logstash
+    docker-compose up --exit-code-from logstash
 else
-    docker-compose -f ci/docker-compose.yml up --exit-code-from logstash logstash
+    docker-compose up --exit-code-from logstash logstash
 fi
--- .ci/logstash-run.sh
+++ .ci/logstash-run.sh
@@ -3,21 +3,20 @@ set -ex
 
 export PATH=$BUILD_DIR/gradle/bin:$PATH
 
+if [[ "$SECURE_INTEGRATION" == "true" ]]; then
+  ES_URL="https://elasticsearch:9200 -k"
+else
+  ES_URL="http://elasticsearch:9200"
+fi
+
 wait_for_es() {
-  echo "Waiting for elasticsearch to respond..."
-  es_url="http://elasticsearch:9200"
-  if [[ "$SECURE_INTEGRATION" == "true" ]]; then
-    es_url="https://elasticsearch:9200 -k"
-  fi
   count=120
-  while ! curl --silent $es_url && [[ $count -ne 0 ]]; do
+  while ! curl -s $ES_URL >/dev/null && [[ $count -ne 0 ]]; do
     count=$(( $count - 1 ))
-    [[ $count -eq 0 ]] && return 1
+    [[ $count -eq 0 ]] && exit 1
     sleep 1
   done
-  echo "Elasticsearch is Up !"
-
-  return 0
+  echo $(curl -s $ES_URL | python2 -c "import sys, json; print json.load(sys.stdin)['version']['number']")
 }
 
 if [[ "$INTEGRATION" != "true" ]]; then
@@ -35,6 +34,8 @@ else
   elif [[ "$DISTRIBUTION" == "default" ]]; then
     extra_tag_args="$extra_tag_args --tag ~distribution:oss --tag distribution:xpack"
   fi
-  wait_for_es  
-  bundle exec rspec -fd $extra_tag_args --tag update_tests:painless --tag update_tests:groovy --tag es_version:$ELASTIC_STACK_VERSION spec/integration
+  echo "Waiting for elasticsearch to respond..."
+  ES_VERSION=$(wait_for_es)
+  echo "Elasticsearch $ES_VERSION is Up!"
+  bundle exec rspec -fd $extra_tag_args --tag update_tests:painless --tag update_tests:groovy --tag es_version:$ES_VERSION spec/integration
 fi
