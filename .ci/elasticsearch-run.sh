66184d68c86945f07d9c22c63391b1298463a2fc
andsel <selva.andre@gmail.com>
2021-09-10 14:08:12 +0200

    Enable SECURE_INTEGRATION for stack version 8 because ES 8 has security enabled by default

--- .ci/elasticsearch-run.sh
+++ .ci/elasticsearch-run.sh
@@ -2,4 +2,3 @@
 set -ex
 
 /usr/share/elasticsearch/bin/elasticsearch -Ediscovery.type=single-node -Eaction.destructive_requires_name=false
-#/usr/share/elasticsearch/bin/elasticsearch -Expack.security.enabled=false -Ediscovery.type=single-node -Eaction.destructive_requires_name=false
