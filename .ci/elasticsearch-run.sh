e27bd6c8645e6ce73f1a3270d47e40c92e16d1e8
andsel <selva.andre@gmail.com>
2021-09-10 13:49:52 +0200

    Disable security on Elasticsearch cluster

--- .ci/elasticsearch-run.sh
+++ .ci/elasticsearch-run.sh
@@ -2,3 +2,4 @@
 set -ex
 
 /usr/share/elasticsearch/bin/elasticsearch -Ediscovery.type=single-node -Eaction.destructive_requires_name=false
+#/usr/share/elasticsearch/bin/elasticsearch -Expack.security.enabled=false -Ediscovery.type=single-node -Eaction.destructive_requires_name=false
