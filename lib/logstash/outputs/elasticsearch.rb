3a3bb02afb19227c67bb85ab0c97797b9989e8f7
andsel <selva.andre@gmail.com>
2022-08-26 16:43:19 +0200

    Fixed bad reassignment to a config setting instead of underlying field

--- lib/logstash/outputs/elasticsearch.rb
+++ lib/logstash/outputs/elasticsearch.rb
@@ -269,7 +269,7 @@ class LogStash::Outputs::ElasticSearch < LogStash::Outputs::Base
     if !failure_type_logging_whitelist.empty?
       @deprecation_logger.deprecated "'failure_type_logging_whitelist' is deprecated and in a future version of " +
         "Elasticsearch output plugin will be removed, please use 'log_silenced_errors' instead."
-      log_silenced_errors = log_silenced_errors | failure_type_logging_whitelist
+      @log_silenced_errors = log_silenced_errors | failure_type_logging_whitelist
     end
 
     @after_successful_connection_done = Concurrent::AtomicBoolean.new(false)
