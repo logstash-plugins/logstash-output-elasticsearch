f5397d9048e0d628ad8b1a385a772bcd970e6c68
andsel <selva.andre@gmail.com>
2022-08-30 11:29:32 +0200

    Log deprecation of setting both on depreation logger and Logstash logger

--- lib/logstash/outputs/elasticsearch.rb
+++ lib/logstash/outputs/elasticsearch.rb
@@ -267,8 +267,10 @@ class LogStash::Outputs::ElasticSearch < LogStash::Outputs::Base
 
   def register
     if !failure_type_logging_whitelist.empty?
-      @deprecation_logger.deprecated "'failure_type_logging_whitelist' is deprecated and in a future version of " +
-        "Elasticsearch output plugin will be removed, please use 'silence_errors_in_log' instead."
+      log_message = "'failure_type_logging_whitelist' is deprecated and in a future version of Elasticsearch " +
+        "output plugin will be removed, please use 'silence_errors_in_log' instead."
+      @deprecation_logger.deprecated log_message
+      @logger.warn log_message
       @silence_errors_in_log = silence_errors_in_log | failure_type_logging_whitelist
     end
 
