c259e1942dcbb87f421f156ecb2c2f1141e7c41f
Mirko Bez <herrBez@users.noreply.github.com>
2023-03-21 17:40:00 +0100

    Fix #1120: Get rid of warning message about unreleased ECS schema v8

--- lib/logstash/outputs/elasticsearch.rb
+++ lib/logstash/outputs/elasticsearch.rb
@@ -349,11 +349,6 @@ class LogStash::Outputs::ElasticSearch < LogStash::Outputs::Base
 
     @bulk_request_metrics = metric.namespace(:bulk_requests)
     @document_level_metrics = metric.namespace(:documents)
-
-    if ecs_compatibility == :v8
-      @logger.warn("Elasticsearch Output configured with `ecs_compatibility => v8`, which resolved to an UNRELEASED preview of version 8.0.0 of the Elastic Common Schema. " +
-                   "Once ECS v8 and an updated release of this plugin are publicly available, you will need to update this plugin to resolve this warning.")
-    end
   end
 
   # @override post-register when ES connection established
