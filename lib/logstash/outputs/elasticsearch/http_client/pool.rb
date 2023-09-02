fbc11518be0363c93c9cd95a120dad9cb92e6435
Kaise Cheng <kaise.cheng@elastic.co>
2023-09-02 01:55:52 +0100

    remove nil check

--- lib/logstash/outputs/elasticsearch/http_client/pool.rb
+++ lib/logstash/outputs/elasticsearch/http_client/pool.rb
@@ -335,7 +335,7 @@ module LogStash; module Outputs; class ElasticSearch; class HttpClient;
     end
 
     def perform_request_to_url(url, method, path, params={}, body=nil)
-      params[:headers] = DEFAULT_EAV_HEADER.merge(params[:headers] || {}) if params && serverless?
+      params[:headers] = DEFAULT_EAV_HEADER.merge(params[:headers] || {}) if serverless?
       @adapter.perform_request(url, method, path, params, body)
     end
 
