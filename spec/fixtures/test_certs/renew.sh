c0525077319c43eb36d0fc87f6366d9af3afc5f2
kaisecheng <69120390+kaisecheng@users.noreply.github.com>
2024-06-25 22:37:04 +0100

    [CI] renew test certs (#1182)
    Fixed: #1183

--- spec/fixtures/test_certs/renew.sh
+++ spec/fixtures/test_certs/renew.sh
@@ -11,3 +11,6 @@ openssl x509 -x509toreq -in test.crt -signkey test.key -out test.csr
 openssl x509 -req -days 365 -in test.csr -set_serial 0x01 -CA ca.crt -CAkey ca.key -out test.crt && rm test.csr
 openssl x509 -in test.crt -outform der | sha256sum | awk '{print $1}' > test.der.sha256
 openssl pkcs12 -export -inkey test.key -in test.crt -passout "pass:1234567890" -out test.p12
+
+# output ISO8601 timestamp to file
+date -Iseconds > GENERATED_AT
