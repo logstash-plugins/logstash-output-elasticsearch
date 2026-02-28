d633c0c1efa3ccb1bb1885e213900cd8ad9ce168
donoghuc <cas.donoghue@gmail.com>
2026-02-27 16:04:22 -0800

    more exploaration about hanging tests

--- .ci/docker-run.sh
+++ .ci/docker-run.sh
@@ -9,5 +9,7 @@ if [ "$INTEGRATION" == "true" ]; then
     # remove the `--attach logstash` if you want to see all logs including elasticsearch container logs
     docker compose up --exit-code-from logstash --attach logstash
 else
-    docker compose up --exit-code-from logstash logstash
+    # Use `docker compose run` for unit tests to avoid Docker Compose v2
+    # `--exit-code-from` bugs with stdout buffering and container exit detection.
+    docker compose run --rm -T logstash
 fi
